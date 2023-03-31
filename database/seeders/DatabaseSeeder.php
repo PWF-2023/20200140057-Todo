9d6f744fde07f759d9098e660f3742a7134a6296
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2023-03-31 15:24:25 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+                'remember_token' => Str::random(10),
+                'is_admin' => true
+            ]
+        );
+        User::create(
+            [
+                'name' => 'Annisa Qurrota Ayun',
+                'email' => 'annisa.qurrota.ft20@mail.umy.ac.id',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+                'remember_token' => Str::random(10),
+                'is_admin' => false
+            ]
+        );
+        User::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
