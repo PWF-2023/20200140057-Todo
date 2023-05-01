d46e796615e73b9e2d5424e7fd83abdc5b74d6c7
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2023-05-01 17:01:38 +0700

    update, delete data

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -87,7 +87,7 @@
                                 <p>{{ $user->todos->count() }}
                                     <span>
                                         <span
-                                            class="text-green-600 dark:text-gray-400">({{ $user->todos->where('is_complete', true)->count() }}</span>
+                                            class="text-green-600 dark:text-green-400">({{ $user->todos->where('is_complete', true)->count() }}</span>/
                                         <span
                                             class="text-blue-600 dark:text-blue-400">{{ $user->todos->where('is_complete', false)->count() }})
                                         </span>
@@ -97,6 +97,33 @@
                             <td class="px-6 py-4">
                                 <div class="flex space-x-3">
                                     {{-- Action Here --}}
+                                    @if ($user->is_admin)
+                                    <form action="{{ route('user.removeadmin', $user) }}" method="Post">
+                                        @csrf
+                                        @method('PATCH')
+                                        <button type="submit"
+                                           class="text-blue-600 dark:text-blue-400 whitespace-nowrap">
+                                                                                    Remove Admin
+                                        </button>
+                                    </form>
+                                    @else
+                                    <form action="{{ route('user.makeadmin', $user) }}" method="Post">
+                                        @csrf
+                                        @method('PATCH')
+                                        <button type="submit"
+                                            class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                                                                                    Make Admin
+                                        </button>
+                                    </form>
+                                    @endif
+                                    <form action="{{ route('user.destroy', $user) }}" method="Post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit"
+                                            class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                                                                                    Delete
+                                        </button>
+                                    </form>
                                 </div>
                             </td>
                         </tr>
---
+++ tugas6.md
@@ -0,0 +1,18 @@
+# Tugas 6
+
+## 1. Todo Completed
+![Alt text](screenshot/tugas6/Screenshot%20(289).png)
+## 2. Todo Uncompleted
+![Alt text](screenshot/tugas6/Screenshot%20(290).png)
+## 3. Todo Edited
+![Alt text](screenshot/tugas6/Screenshot%20(291).png)
+## 4. Todo Deleted
+![Alt text](screenshot/tugas6/Screenshot%20(292).png)
+## 5. Todo Delete All Completed Todo
+![Alt text](screenshot/tugas6/Screenshot%20(293).png)
+## 6. User Make Admin
+![Alt text](screenshot/tugas6/Screenshot%20(294).png)
+## 7. User Remove Admin
+![Alt text](screenshot/tugas6/Screenshot%20(295).png)
+## 8. User Deleted
+![Alt text](screenshot/tugas6/Screenshot%20(296).png)
